f82e8e319b3c66d7c00b62586bee8f743bc1ae47
EmekaEmmanuel <emekaekeohaa@gmail.com>
2023-01-15 11:07:00 -0800

    ADD changes to singlepage.js

--- modules/usableData.js
+++ modules/usableData.js
@@ -3,6 +3,8 @@ const author = document.querySelector('.author_input');
 const empty = document.querySelector('.empty_bookstore');
 const form = document.querySelector('.book_form');
 const tableBody = document.querySelector('.table_body'); 
+const tabBtns = document.querySelectorAll('.operations_tab');
+const operationsContents = document.querySelectorAll('.operations_content'); 
 const storedBooks = JSON.parse(localStorage.getItem('bookstored')); 
 
-export {title, author, empty, form, tableBody, storedBooks}
+export {title, author, empty, form, tableBody, storedBooks, tabBtns, operationsContents}
--- singlepage.js
+++ singlepage.js
@@ -1,22 +1,6 @@
-import Book from "./modules/book.js"
-import {AwesomeLibrary} from "./modules/awesomeLibrary.js"
-console.log(Book);
-console.log(AwesomeLibrary);
-// const title = document.querySelector('.title_input');
-// const author = document.querySelector('.author_input');
-// const empty = document.querySelector('.empty_bookstore');
-// const form = document.querySelector('.book_form');
-// const tableBody = document.querySelector('.table_body');
-const tabBtns = document.querySelectorAll('.operations_tab');
-const operationsContents = document.querySelectorAll('.operations_content');
-// const storedBooks = JSON.parse(localStorage.getItem('bookstored')); 
-import {form} from './modules/usableData.js'
-
-tabBtns.forEach(tabBtn => {
-    tabBtn.addEventListener('click', ()=>{
-        displayTabSection(tabBtn)
-    })
-})
+import {AwesomeLibrary} from "./modules/awesomeLibrary.js";
+import {form, tabBtns, operationsContents} from './modules/usableData.js';
+const newLibrary = new AwesomeLibrary();
 
 const displayTabSection = (selectedTab)=>{
     let clickedContent = document.querySelector(`.tab_content_${selectedTab.dataset.tab}`)
@@ -28,56 +12,18 @@ const displayTabSection = (selectedTab)=>{
         clickedContent.classList.add('active');
         clickedContent.classList.remove('hide');
     })
+} 
+
+const clearInputFields = (title, author) => {
+    title.value = '';
+    author.value = '';
 }
 
-// class AwesomeLibrary {
-//     constructor() {
-//         this.storedBooks = storedBooks;
-//     }
-
-//     addBookDetails() {
-//         const booksData = new Book(title.value, author.value);
-//         if (!this.storedBooks) {
-//             this.storedBooks = [];
-//         }
-//         this.storedBooks.push(booksData);
-//         localStorage.setItem('bookstored', JSON.stringify(this.storedBooks));
-//         this.displayBooks();
-//     }
-
-//     displayBooks() {
-//         if (!this.storedBooks) {
-//             empty.innerHTML = 'No books in the library';
-//         } else {
-//             empty.style.display = 'none';
-//             let eachTableRow = '';
-//             for (let i = 0; i < this.storedBooks.length; i += 1) {
-//                 eachTableRow
-//                     += `<tr>
-//                 <td>"${this.storedBooks[i].title}" by ${this.storedBooks[i].author}</td>
-//                 <td><button type="button" id=${i} class="remove_btn">Remove</button></td>
-//             </tr>
-//             `;
-//             }
-//             tableBody.innerHTML = eachTableRow;
-//             const deleteBookBtn = document.querySelectorAll('.remove_btn');
-//             this.removeBooks(deleteBookBtn);
-//         }
-//     }
-
-//     removeBooks(deleteBookBtn) {
-//         deleteBookBtn.forEach((elementBtn) => {
-//             elementBtn.addEventListener('click', () => {
-//                 const index = elementBtn.getAttribute('id');
-//                 this.storedBooks.splice(index, 1);
-//                 localStorage.setItem('bookstored', JSON.stringify(this.storedBooks));
-//                 this.displayBooks();
-//             });
-//         });
-//     }
-// }
-
-const newLibrary = new AwesomeLibrary();
+tabBtns.forEach(tabBtn => {
+    tabBtn.addEventListener('click', ()=>{
+        displayTabSection(tabBtn)
+    })
+})
 
 window.addEventListener('load', () => {
     newLibrary.displayBooks();
@@ -85,11 +31,6 @@ window.addEventListener('load', () => {
     const clock = document.querySelector('.date_time');
     clock.innerHTML = date;
 });
-
-function clearInputFields(title, author) {
-    title.value = '';
-    author.value = '';
-}
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
