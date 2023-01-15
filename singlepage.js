d5e02e4b3509405a78fcdfce9e459c62d9b60093
EmekaEmmanuel <emekaekeohaa@gmail.com>
2023-01-15 10:44:07 -0800

    Add changes to singlepages.js, and models directory

--- singlepage.js
+++ singlepage.js
@@ -1,75 +1,81 @@
 import Book from "./modules/book.js"
-const title = document.querySelector('.title_input');
-const author = document.querySelector('.author_input');
-const empty = document.querySelector('.empty_bookstore');
-const form = document.querySelector('.book_form');
-const tableBody = document.querySelector('.table_body');
+import {AwesomeLibrary} from "./modules/awesomeLibrary.js"
+console.log(Book);
+console.log(AwesomeLibrary);
+// const title = document.querySelector('.title_input');
+// const author = document.querySelector('.author_input');
+// const empty = document.querySelector('.empty_bookstore');
+// const form = document.querySelector('.book_form');
+// const tableBody = document.querySelector('.table_body');
 const tabBtns = document.querySelectorAll('.operations_tab');
 const operationsContents = document.querySelectorAll('.operations_content');
-const storedBooks = JSON.parse(localStorage.getItem('bookstored'));
-console.log(storedBooks.length);
+// const storedBooks = JSON.parse(localStorage.getItem('bookstored')); 
+import {form} from './modules/usableData.js'
 
 tabBtns.forEach(tabBtn => {
-    tabBtn.addEventListener('click', () => {
-        let clickedContent = document.querySelector(`.tab_content_${tabBtn.dataset.tab}`)
-        operationsContents.forEach(operationsContent => {
-            if (clickedContent !== operationsContent) {
-                operationsContent.classList.remove('active');
-                operationsContent.classList.add('hide');
-            }
-            clickedContent.classList.add('active');
-            clickedContent.classList.remove('hide');
-        })
+    tabBtn.addEventListener('click', ()=>{
+        displayTabSection(tabBtn)
     })
 })
 
-
-class AwesomeLibrary {
-    constructor() {
-        this.storedBooks = storedBooks;
-    }
-
-    addBookDetails() {
-        const booksData = new Book(title.value, author.value);
-        if (!this.storedBooks) {
-            this.storedBooks = [];
+const displayTabSection = (selectedTab)=>{
+    let clickedContent = document.querySelector(`.tab_content_${selectedTab.dataset.tab}`)
+    operationsContents.forEach(operationsContent => {
+        if (clickedContent !== operationsContent) {
+            operationsContent.classList.remove('active');
+            operationsContent.classList.add('hide');
         }
-        this.storedBooks.push(booksData);
-        localStorage.setItem('bookstored', JSON.stringify(this.storedBooks));
-        this.displayBooks();
-    }
-
-    displayBooks() {
-        if (!this.storedBooks) {
-            empty.innerHTML = 'No books in the library';
-        } else {
-            empty.style.display = 'none';
-            let eachTableRow = '';
-            for (let i = 0; i < this.storedBooks.length; i += 1) {
-                eachTableRow
-                    += `<tr>
-                <td>"${this.storedBooks[i].title}" by ${this.storedBooks[i].author}</td>
-                <td><button type="button" id=${i} class="remove_btn">Remove</button></td>
-            </tr>
-            `;
-            }
-            tableBody.innerHTML = eachTableRow;
-            const deleteBookBtn = document.querySelectorAll('.remove_btn');
-            this.removeBooks(deleteBookBtn);
-        }
-    }
-
-    removeBooks(deleteBookBtn) {
-        deleteBookBtn.forEach((elementBtn) => {
-            elementBtn.addEventListener('click', () => {
-                const index = elementBtn.getAttribute('id');
-                this.storedBooks.splice(index, 1);
-                localStorage.setItem('bookstored', JSON.stringify(this.storedBooks));
-                this.displayBooks();
-            });
-        });
-    }
+        clickedContent.classList.add('active');
+        clickedContent.classList.remove('hide');
+    })
 }
+
+// class AwesomeLibrary {
+//     constructor() {
+//         this.storedBooks = storedBooks;
+//     }
+
+//     addBookDetails() {
+//         const booksData = new Book(title.value, author.value);
+//         if (!this.storedBooks) {
+//             this.storedBooks = [];
+//         }
+//         this.storedBooks.push(booksData);
+//         localStorage.setItem('bookstored', JSON.stringify(this.storedBooks));
+//         this.displayBooks();
+//     }
+
+//     displayBooks() {
+//         if (!this.storedBooks) {
+//             empty.innerHTML = 'No books in the library';
+//         } else {
+//             empty.style.display = 'none';
+//             let eachTableRow = '';
+//             for (let i = 0; i < this.storedBooks.length; i += 1) {
+//                 eachTableRow
+//                     += `<tr>
+//                 <td>"${this.storedBooks[i].title}" by ${this.storedBooks[i].author}</td>
+//                 <td><button type="button" id=${i} class="remove_btn">Remove</button></td>
+//             </tr>
+//             `;
+//             }
+//             tableBody.innerHTML = eachTableRow;
+//             const deleteBookBtn = document.querySelectorAll('.remove_btn');
+//             this.removeBooks(deleteBookBtn);
+//         }
+//     }
+
+//     removeBooks(deleteBookBtn) {
+//         deleteBookBtn.forEach((elementBtn) => {
+//             elementBtn.addEventListener('click', () => {
+//                 const index = elementBtn.getAttribute('id');
+//                 this.storedBooks.splice(index, 1);
+//                 localStorage.setItem('bookstored', JSON.stringify(this.storedBooks));
+//                 this.displayBooks();
+//             });
+//         });
+//     }
+// }
 
 const newLibrary = new AwesomeLibrary();
 
